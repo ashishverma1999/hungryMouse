e5b685325eb4b1bddeb8b32866dd23f00888442c
Ashish Verma <ashishav8055@gmail.com>
2026-08-08 21:02:36 +0530

    Fix build script to include HTML files

--- gulp/build.gulp.js
+++ gulp/build.gulp.js
@@ -4,7 +4,7 @@ const { paths, version } = require('./utils.js');
 /* -------------------------------------------------------------------------- */
 /*                                    Build                                   */
 /* -------------------------------------------------------------------------- */
-gulp.task('build:static', () => gulp.src(paths.watch.map(dir => `${dir}/**/*`), {
+gulp.task('build:static', () => gulp.src([...paths.watch.map(dir => `${dir}/**/*`), '*.html'], {
   cwd: paths.dir.dev,
   base: `./${paths.dir.dev}`
 })
