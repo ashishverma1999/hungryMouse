fb631ae3d009545f313b24092a160d102d4294bd
Ashish Verma <ashishav8055@gmail.com>
2026-08-08 20:26:12 +0530

    allignment and look changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,66 +1,66 @@
 <!DOCTYPE html>
 <html lang="en-US" dir="ltr">
 
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <!-- ===============================================-->
-    <!--    Document Title-->
-    <!-- ===============================================-->
-    <title>Hungry Mouse | Cloud Kitchen • On The Move</title>
+  <!-- ===============================================-->
+  <!--    Document Title-->
+  <!-- ===============================================-->
+  <title>Hungry Mouse | Cloud Kitchen • On The Move</title>
 
 
-    <!-- ===============================================-->
-    <!--    Favicons-->
-    <!-- ===============================================-->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset(\'assets/img/gallery/hungry mouse.png\') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset(\'assets/img/gallery/hungry mouse.png\') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset(\'assets/img/gallery/hungry mouse.png\') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset(\'assets/img/gallery/hungry mouse.png\') }}">
-    <link rel="manifest" href="{{ asset(\'assets/img/favicons/manifest.json\') }}">
-    <meta name="msapplication-TileImage" content="assets/img/gallery/hungry mouse.png">
-    <meta name="theme-color" content="#ffffff">
+  <!-- ===============================================-->
+  <!--    Favicons-->
+  <!-- ===============================================-->
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset(\'assets/img/gallery/hungrymouseicon.png\') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset(\'assets/img/gallery/hungrymouseicon.png\') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset(\'assets/img/gallery/hungrymouseicon.png\') }}">
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset(\'assets/img/gallery/hungrymouseicon.png\') }}">
+  <link rel="manifest" href="{{ asset(\'assets/img/favicons/manifest.json\') }}">
+  <meta name="msapplication-TileImage" content="assets/img/gallery/hungrymouseicon.png">
+  <meta name="theme-color" content="#ffffff">
 
 
-    <!-- ===============================================-->
-    <!--    Stylesheets-->
-    <!-- ===============================================-->
-    <link href="{{ asset(\'assets/css/theme.css\') }}" rel="stylesheet" />
+  <!-- ===============================================-->
+  <!--    Stylesheets-->
+  <!-- ===============================================-->
+  <link href="{{ asset(\'assets/css/theme.css\') }}" rel="stylesheet" />
 
-  </head>
+</head>
 
 
-  <body>
+<body>
 
-    <!-- ===============================================-->
-    <!--    Main Content-->
-    <!-- ===============================================-->
-    <main class="main" id="top">
-      
-<x-navbar />
+  <!-- ===============================================-->
+  <!--    Main Content-->
+  <!-- ===============================================-->
+  <main class="main" id="top">
+
+    <x-navbar />
     @yield('content')
-<x-footer />
-</main>
-    <!-- ===============================================-->
-    <!--    End of Main Content-->
-    <!-- ===============================================-->
+    <x-footer />
+  </main>
+  <!-- ===============================================-->
+  <!--    End of Main Content-->
+  <!-- ===============================================-->
 
 
 
 
-    <!-- ===============================================-->
-    <!--    JavaScripts-->
-    <!-- ===============================================-->
-    <script src="{{ asset(\'vendors/@popperjs/popper.min.js\') }}"></script>
-    <script src="{{ asset(\'vendors/bootstrap/bootstrap.min.js\') }}"></script>
-    <script src="{{ asset(\'vendors/is/is.min.js\') }}"></script>
-    <script src="{{ asset(\'vendors/fontawesome/all.min.js\') }}"></script>
-    <script src="{{ asset(\'assets/js/theme.js\') }}"></script>
+  <!-- ===============================================-->
+  <!--    JavaScripts-->
+  <!-- ===============================================-->
+  <script src="{{ asset(\'vendors/@popperjs/popper.min.js\') }}"></script>
+  <script src="{{ asset(\'vendors/bootstrap/bootstrap.min.js\') }}"></script>
+  <script src="{{ asset(\'vendors/is/is.min.js\') }}"></script>
+  <script src="{{ asset(\'vendors/fontawesome/all.min.js\') }}"></script>
+  <script src="{{ asset(\'assets/js/theme.js\') }}"></script>
 
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&amp;display=swap" rel="stylesheet">
-  </body>
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&amp;display=swap" rel="stylesheet">
+</body>
 
 </html>
